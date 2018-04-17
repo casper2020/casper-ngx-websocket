78511af3092b437b039653feffa520e915d0c462
cloudware <cloudware@staging.local>
2018-04-17 16:29:04 +0100

    Mem leak fix back again - not guilty

--- src/ngx/ws/websocket_context.cc
+++ src/ngx/ws/websocket_context.cc
@@ -46,6 +46,5 @@ ngx::ws::Context::Context (ngx_module_t& a_module, ngx_http_request_t* a_http_re
  */
 ngx::ws::Context::~Context ()
 {
-    // TMP: disabled while searching for crash
-    // delete client_;
+    delete client_;
 }
